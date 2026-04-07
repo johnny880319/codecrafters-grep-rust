20563cf495caa9e7e25369545c9c2155da87504f
= <=>
2026-04-07 20:27:05 +0800

    codecrafters submit [skip ci]

--- src/pattern.rs
+++ src/pattern.rs
@@ -6,49 +6,10 @@ pub fn match_pattern(input_line: &str, pattern: &str) -> Result<bool> {
         let mut pattern_idx = 0;
         while input_idx < input_line.len() && pattern_idx < pattern.len() {
             let input_char = input_line.as_bytes()[input_idx] as char;
-
-            match &pattern[pattern_idx..] {
-                p if p.starts_with("\\d") => {
-                    if !input_char.is_ascii_digit() {
-                        break;
-                    }
-                    pattern_idx += 2;
-                }
-                p if p.starts_with("\\w") => {
-                    if !input_char.is_ascii_alphanumeric() && input_char != '_' {
-                        break;
-                    }
-                    pattern_idx += 2;
-                }
-                p if p.starts_with("[^") && p.contains(']') => {
-                    let end_idx = pattern[pattern_idx..]
-                        .find(']')
-                        .ok_or_else(|| anyhow::anyhow!("Unmatched [ in pattern"))?
-                        + pattern_idx;
-                    let chars_to_not_match = &pattern[pattern_idx + 2..end_idx];
-                    if chars_to_not_match.contains(input_char) {
-                        break;
-                    }
-                    pattern_idx = end_idx + 1;
-                }
-                p if p.starts_with('[') && p.contains(']') => {
-                    let end_idx = pattern[pattern_idx..]
-                        .find(']')
-                        .ok_or_else(|| anyhow::anyhow!("Unmatched [ in pattern"))?
-                        + pattern_idx;
-                    let chars_to_match = &pattern[pattern_idx + 1..end_idx];
-                    if !chars_to_match.contains(input_char) {
-                        break;
-                    }
-                    pattern_idx = end_idx + 1;
-                }
-                _ => {
-                    let pattern_char = pattern.as_bytes()[pattern_idx] as char;
-                    if input_char != pattern_char {
-                        break;
-                    }
-                    pattern_idx += 1;
-                }
+            let is_match;
+            (is_match, pattern_idx) = match_char(input_char, pattern, pattern_idx)?;
+            if !is_match {
+                break;
             }
             input_idx += 1;
         }
@@ -57,4 +18,50 @@ pub fn match_pattern(input_line: &str, pattern: &str) -> Result<bool> {
         }
     }
     Ok(false)
+}
+
+fn match_char(input_char: char, pattern: &str, pattern_idx: usize) -> Result<(bool, usize)> {
+    match &pattern[pattern_idx..] {
+        p if p.starts_with("\\d") => {
+            if !input_char.is_ascii_digit() {
+                return Ok((false, pattern_idx));
+            }
+            Ok((true, pattern_idx + 2))
+        }
+        p if p.starts_with("\\w") => {
+            if !input_char.is_ascii_alphanumeric() && input_char != '_' {
+                return Ok((false, pattern_idx));
+            }
+            Ok((true, pattern_idx + 2))
+        }
+        p if p.starts_with("[^") && p.contains(']') => {
+            let end_idx = pattern[pattern_idx..]
+                .find(']')
+                .ok_or_else(|| anyhow::anyhow!("Unmatched [ in pattern"))?
+                + pattern_idx;
+            let chars_to_not_match = &pattern[pattern_idx + 2..end_idx];
+            if chars_to_not_match.contains(input_char) {
+                return Ok((false, pattern_idx));
+            }
+            Ok((true, end_idx + 1))
+        }
+        p if p.starts_with('[') && p.contains(']') => {
+            let end_idx = pattern[pattern_idx..]
+                .find(']')
+                .ok_or_else(|| anyhow::anyhow!("Unmatched [ in pattern"))?
+                + pattern_idx;
+            let chars_to_match = &pattern[pattern_idx + 1..end_idx];
+            if !chars_to_match.contains(input_char) {
+                return Ok((false, pattern_idx));
+            }
+            Ok((true, end_idx + 1))
+        }
+        _ => {
+            let pattern_char = pattern.as_bytes()[pattern_idx] as char;
+            if input_char != pattern_char {
+                return Ok((false, pattern_idx));
+            }
+            Ok((true, pattern_idx + 1))
+        }
+    }
 }
