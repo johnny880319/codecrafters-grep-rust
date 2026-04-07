c6268e0a5988cd57ca073799e15db1bf58cfc6a0
= <=>
2026-04-07 18:57:01 +0800

    codecrafters submit [skip ci]

--- src/pattern.rs
+++ src/pattern.rs
@@ -9,7 +9,7 @@ pub fn match_pattern(input_line: &str, pattern: &str) -> bool {
     }
     if pattern.starts_with("[^") && pattern.ends_with(']') {
         let chars_to_not_match = &pattern[2..pattern.len() - 1];
-        return input_line.chars().all(|c| !chars_to_not_match.contains(c));
+        return input_line.chars().any(|c| !chars_to_not_match.contains(c));
     }
     if pattern.starts_with('[') && pattern.ends_with(']') {
         let chars_to_match = &pattern[1..pattern.len() - 1];
