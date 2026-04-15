8b03dbaebbcd96cd8bf411c9005d3927879145fd
= <=>
2026-04-15 14:52:19 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+use crate::args::GrepArgs;
 use crate::pattern::CompiledPattern;
 use anyhow::Result;
 use std::{
-    env, fs,
-    io::{self, IsTerminal, Read},
+    fs,
+    io::{self, Read},
     process,
 };
-use walkdir::WalkDir;
+
+mod args;
 mod output;
 mod pattern;
 
 // Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() -> Result<()> {
-    let grep_args = parse_args();
+    let grep_args = args::parse_args();
     let compiled_pattern = CompiledPattern::parse(&grep_args.pattern_text)?;
 
     let mut is_any_match = false;
@@ -33,71 +35,6 @@ fn main() -> Result<()> {
         process::exit(1);
     }
     process::exit(0)
-}
-
-struct GrepArgs {
-    pattern_text: String,
-    file_paths: Vec<String>,
-    print_file_name: bool,
-    only_matching: bool,
-    color_mode: bool,
-}
-
-fn parse_args() -> GrepArgs {
-    let env_args: Vec<String> = env::args().collect();
-    let only_matching = env_args.iter().any(|arg| arg == "-o");
-
-    let is_color_always = env_args.iter().any(|arg| arg == "--color=always");
-    let is_color_auto = env_args.iter().any(|arg| arg == "--color=auto");
-    let color_mode = is_color_always || (is_color_auto && io::stdout().is_terminal());
-
-    // First argument that is not a flag is the pattern
-    let pattern_text = env_args
-        .iter()
-        .skip(1)
-        .find(|arg| !arg.starts_with('-'))
-        .cloned()
-        .unwrap_or_else(|| {
-            eprintln!("Error: No pattern provided");
-            process::exit(1);
-        });
-
-    // Second argument that is not a flag is file or directory path
-    let file_or_dir_paths = env_args
-        .iter()
-        .filter(|p| !p.starts_with('-'))
-        .skip(2)
-        .collect::<Vec<_>>();
-
-    // If -r flag is provided, we need to recursively search for files in the provided directories
-    let r_flag = env_args.iter().any(|arg| arg == "-r");
-    let print_file_name = r_flag || file_or_dir_paths.len() > 1;
-
-    let file_paths = if r_flag {
-        let mut paths = Vec::new();
-        for path in file_or_dir_paths {
-            if fs::metadata(path).map(|m| m.is_dir()).unwrap_or(false) {
-                for entry in WalkDir::new(path).follow_links(false).into_iter().flatten() {
-                    if entry.file_type().is_file() {
-                        paths.push(entry.path().to_string_lossy().to_string());
-                    }
-                }
-            } else {
-                paths.push(path.clone());
-            }
-        }
-        paths
-    } else {
-        file_or_dir_paths.into_iter().cloned().collect()
-    };
-
-    GrepArgs {
-        pattern_text,
-        file_paths,
-        print_file_name,
-        only_matching,
-        color_mode,
-    }
 }
 
 fn match_content(
